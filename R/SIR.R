98416277551fee79fdf70c94ff2da8fd924770f0
Clément Weinreich <clement@weinreich.fr>
2022-06-23 09:55:34 +0200

    rename and snake_case on SIR

--- R/SIR.R
+++ R/SIR.R
@@ -37,68 +37,68 @@ SIR <- function(Y, X, H = 10) {
     p <- ncol(X)
 
     # Moyenne des échantillons pour chaque variable de X
-    moy.X <- matrix(apply(X, 2, mean), ncol = 1)
+    moy_X <- matrix(apply(X, 2, mean), ncol = 1)
 
     # Calcul de la matrice de covariance de X
-    Sigma <- var(X) * (n - 1) / n
+    sigma <- var(X) * (n - 1) / n
 
     # Ordonne les données de X à partir des indices triés dans l'ordre croissant de Y
-    X.ord <- X[order(Y),, drop = FALSE]
+    X_ord <- X[order(Y),, drop = FALSE]
 
     # le vecteur nH contient le nombre d'observation pour chaque tranche H
-    vect.nh <- rep(n %/% H, H) # %/% = integer division
+    vect_nh <- rep(n %/% H, H) # %/% = integer division
 
     # si le nombre d'observations total est plus grand que le nb d'observation 
     # dans vect.nh pour prendre le reste de la division de n %/% H 
-    if ((n - sum(vect.nh)) > 0) {
+    if ((n - sum(vect_nh)) > 0) {
         # pour chaque observation non présente dans vect.nh
-        for (i in 1:(n - sum(vect.nh))) {
+        for (i in 1:(n - sum(vect_nh))) {
             # on prend une tranche au hasard
             h <- sample(1:H, 1)
             # on ajoute 1 observation dans cette tranche
-            vect.nh[h] <- vect.nh[h] + 1
+            vect_nh[h] <- vect_nh[h] + 1
         }
     }
 
     # vect.ph = proportion des yi tombant dans chaque tranche
-    vect.ph <- vect.nh / n
+    vect_ph <- vect_nh / n
 
     # séparation des n échantillons en H tranche
     # (on assigne un indice h à chaque échantillon)
-    vect.h <- rep(1:H, times = vect.nh)
+    vect_h <- rep(1:H, times = vect_nh)
 
     # Matrice des moyennes par tranche des xi (matrice p*H)
-    mat.mh <- matrix(0, ncol = H, nrow = p)
+    mat_mh <- matrix(0, ncol = H, nrow = p)
 
     # Pout chaque tranche
     for (h in 1:H) {
         # récupération des indices pour la tranche h
-        indicesTrancheH <- which(vect.h == h)
+        index_slice_h <- which(vect_h == h)
         # récupération des x appartenant à la tranche h
-        xDansTrancheH <- X.ord[indicesTrancheH,, drop = FALSE]
+        x_in_slice_h <- X_ord[index_slice_h,, drop = FALSE]
         # applique la moyenne aux échantillons de chaque feature de la tranche h
-        mat.mh[, h] <- apply(xDansTrancheH, 2, mean)
+        mat_mh[, h] <- apply(x_in_slice_h, 2, mean)
     }
 
     # Estimation de la matrice de covariances des moyennes par tranche
     # (moy.X.etendue pour broadcaster l'opération - entre mat.mh et moy.X)
-    moy.X.etendue <- moy.X %*% matrix(rep(1, H), ncol = H)
-    M <- (mat.mh - moy.X.etendue) %*% diag(vect.ph) %*% t(mat.mh - moy.X.etendue)
+    moy_X_extended <- moy_X %*% matrix(rep(1, H), ncol = H)
+    M <- (mat_mh - moy_X_extended) %*% diag(vect_ph) %*% t(mat_mh - moy_X_extended)
 
     # Matrice d'intérêt : inverse de la matrice de covariance de X multipliée par la
     # matrice de covariance des moyennes par tranche :(pxp) %*% (pxp)
     # matrice d'intérêt =  Sigma^-1 * Cov(Moyenne par tranche)
-    SigmaInv <- solve(Sigma)
-    M1 <- SigmaInv %*% M
+    sigma_inv <- solve(sigma)
+    M1 <- sigma_inv %*% M
 
     # eigen(M1)$vectors donne une matrice pxp qui contient les p vecteurs propres de x 
     # dans chaque colonne. Les vecteurs propres sont retournés dans l'ordre décroissant
     # de leur valeur propre associée. Donc ici on récupère le vecteur propre associé 
     # à la plus grande valeur propre pour avoir b.est donc la première colonne
-    rSIR <- eigen(M1)
-    eig.values <- Re(rSIR$values)
-    eig.vectors <- Re(rSIR$vectors)
-    b <- eig.vectors[, 1]
+    res_SIR <- eigen(M1)
+    eig_values <- Re(res_SIR$values)
+    eig_vectors <- Re(res_SIR$vectors)
+    b <- eig_vectors[, 1]
 
     # conversion en matrice à une ligne et p colonnes
     b <- matrix(b, nrow = 1)
@@ -112,7 +112,7 @@ SIR <- function(Y, X, H = 10) {
     # On a donc b.est l'estimation de la direction de Beta
 
 
-    res <- list(b = b, M1 = M1, eig.val = eig.values,
+    res <- list(b = b, M1 = M1, eig_val = eig_values,
         n = n, p = p, H = H, call = cl)
     class(res) <- "SIR"
 
