948fdb4ae0a0aedb91c425f13b904792b29b48ba
Clément Weinreich <clement@weinreich.fr>
2022-07-04 11:38:15 +0200

    Comment and document sir bootstrpa

--- R/SIR_bootstrap.R
+++ R/SIR_bootstrap.R
@@ -1,18 +1,24 @@
 #'  Bootstrap SIR
 #'
-#' Apply a single-index SIR (Sliced Inverse Regression) on B bootstraped sample of 
+#' Apply a single-index SIR (Sliced Inverse Regression) on B bootstraped samples of 
 # (X,Y) with H slices.
 #' @param X A matrix representing the quantitative explanatory variables (bind by column).
 #' @param Y A numeric vector representing the dependent variable (a response vector).
 #' @param H The chosen number of slices.
 #' @param B The number of bootstrapped sample to draw
+#' @param graphic A boolean that must be set to true to display graphics
+#' @param choice To specify the desired graphic (choose between eigvals and estim_ind)
 #' @return An object of class SIR.bootstrap, with attributes:
-#' \item{beta}{This is an estimated EDR direction, which is the principal eigenvector
+#' \item{b}{This is an estimated EDR direction, which is the principal eigenvector
 #' of the interest matrix.}
-#' \item{mat.b.est}{A matrix of size p*B that contains an estimation of beta in 
+#' \item{mat_b}{A matrix of size p*B that contains an estimation of beta in 
 #' the columns for each bootstrapped sample}
 #' \item{n}{Sample size.}
 #' \item{p}{The number of variables in X.}
+#' \item{H}{The chosen number of slices.}
+#' \item{call}{Unevaluated call to the function}
+#' \item{index_pred}{The index b'X estimated by SIR}
+#' \item{Y}{The response vector}
 #' @examples
 #' # Generate Data
 #' set.seed(10)
@@ -23,34 +29,32 @@
 #' Y <- (X%*%beta)**3+eps
 #'
 #' # Apply bootstrap SIR
-#' SIR_bootstrap(Y,X,H=10,B=10)
+#' SIR_bootstrap(Y, X, H = 10, B = 10)
 #' @export
-SIR_bootstrap <- function(Y, X, H = 10, B = 10,graphic = TRUE,choice="") {
+SIR_bootstrap <- function(Y, X, H = 10, B = 10, graphic = TRUE, choice = "") {
 
     cl <- match.call()
 
     p <- ncol(X)
     n <- nrow(X)
 
-    # Initialisation de la matrice des estimations de beta_hat de taille pxB
-    # avec l'estimation de beta_hat pour le nouvel échantillon après bootstrap dans
-    # les colonnes de la matrice
+    # Initialization of a matrix of size pxB  that will contain the estimation 
+    # of b for each the new sample after bootstrapping in the columns of the matrix
     mat_b <- matrix(0, ncol = B, nrow = p)
     for (r in 1:B) {
-        # indice des éléments à prendre dans X : on tire avec remise les indices
-        # des éléments de X et on créé une liste qui contient ces indices (Bootstrap)
+        # index of the samples to be taken in X : we draw with discount the indices
+        # of X samples and store it in a vector (Bootstrap)
         indice <- sample(1:n, replace = TRUE)
-        # on stock chaque estimation b dans les colonnes de la matrice d'estimation
+        # store each estimate b in the columns of mat_b
         mat_b[, r] <- SIR(Y[indice], X[indice,], H = H, graphic = FALSE)$b
     }
 
-    # on récupère le vecteur propre associé à la plus grande valeur propre
-    # de la matrice d'estimations de beta_hat
-    e = eigen(mat_b %*% t(mat_b), symmetric = TRUE)
+    # we recover the eigenvector associated to the largest eigenvalue of mat_b
+    e <- eigen(mat_b %*% t(mat_b), symmetric = TRUE)
     b <- e$vectors[, 1]
-    eig_values = Re(e$values)
+    eig_values <- Re(e$values)
 
-    # conversion en matrice à une ligne
+    # convert it into a one line matrix
     b <- matrix(b, nrow = 1)
 
     if (!is.null(colnames(X))) {
@@ -58,14 +62,16 @@ SIR_bootstrap <- function(Y, X, H = 10, B = 10,graphic = TRUE,choice="") {
     } else {
         colnames(b) <- paste("X", 1:p, sep = "")
     }
-    
+
+    # compute the estimated index
     index_pred <- X %*% t(b)
 
-    res <- list(b = b, mat_b = mat_b, n = n, p = p, H = H, call = cl,index_pred = index_pred,eig_values=eig_values,Y=Y)
+    res <- list(b = b, mat_b = mat_b, n = n, p = p, H = H, call = cl,
+    index_pred = index_pred, eig_values = eig_values, Y = Y)
     class(res) <- "SIR_bootstrap"
-    
+
     if (graphic) {
-        plot.SIR_bootstrap(res,choice=choice)
+        plot.SIR_bootstrap(res, choice = choice)
     }
 
     return(res)
--- R/plot.SIR_bootstrap.R
+++ R/plot.SIR_bootstrap.R
@@ -1,33 +1,57 @@
+#'  Graphical output of SIR_bootstrap
+#'
+#' Display the 10 first eigen values and the estimated index versus Y of the 
+#' SIRbootstrap model
+#' @param x A SIR_bootstrap object
+#' @param choice the graph to plot: 
+#' \item{eigvals}{Plot the eigen values of the matrix of interest}
+#' \item{estim_ind}{Plot the estimated index by the SIR model versus Y}
+#' @examples
+#' # Generate Data
+#' set.seed(10)
+#' n <- 500
+#' beta <- c(1,1,rep(0,8))
+#' X <- mvtnorm::rmvnorm(n,sigma=diag(1,10))
+#' eps <- rnorm(n)
+#' Y <- (X%*%beta)**3+eps
+#'
+#' # Apply bootstrap SIR
+#' SIR_bootstrap(Y, X, H = 10, B = 10)
+#' 
+#' # Eigen values
+#' plot.SIR_bootstrap(res,choice="eigvals")
+#'
+#' # Estimated index versus Y
+#' plot.SIR_bootstrap(res,choice="estim_ind")
 #' @export
-#' @keywords internal
-plot.SIR_bootstrap <- function(x, choice="",...) {
-    
+plot.SIR_bootstrap <- function(x, choice = "", ...) {
+
     if (!inherits(x, "SIR_bootstrap"))
         stop("Only use with \"SIR_bootstrap\" obects")
-    
-    if (!(choice %in% c("eigvals", "estim_ind",""))) 
-        stop("\"choice\" must be either \"eigvals\" or \"estim_ind\"",call. = FALSE)
-    
-    if(choice=="" || choice=="eigvals"){
+
+    if (!(choice %in% c("eigvals", "estim_ind", "")))
+        stop("\"choice\" must be either \"eigvals\" or \"estim_ind\"", call. = FALSE)
+
+    if (choice == "" || choice == "eigvals") {
         dev.new()
-        # on prend au plus les 10 premières valeurs propres et on les affichent 
-        # dans un screplot
+
         eig_vals <- x$eig_val
         title = "Eigenvalues"
+        # Take only the 10 first eigen values
         if (length(eig_vals) >= 10) {
             eig_vals <- eig_vals[1:10]
             title = "10 first eigenvalues"
         }
-        
-        a=barplot(eig_vals, ylab = "eigenvalues", xlab = "dimensions", main = title)
-        axis(1, at = a, labels=seq(1,length(eig_vals))) 
+
+        a = barplot(eig_vals, ylab = "eigenvalues", xlab = "dimensions", main = title)
+        axis(1, at = a, labels = seq(1, length(eig_vals)))
     }
-    
-    
-    if(choice=="" || choice=="estim_ind"){
+
+
+    if (choice == "" || choice == "estim_ind") {
         dev.new()
         plot(x$index_pred, x$Y, xlab = "Estimated first index", ylab = "y", pch = 4)
         title("Reconstructed index")
     }
-    
+
 }
